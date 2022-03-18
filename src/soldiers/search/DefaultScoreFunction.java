aa2da5f2ead83028bab65ca0022bf3c50177b761
tigersmuseum <85739505+tigersmuseum@users.noreply.github.com>
2022-03-18 12:41:15 +0000

    default is now to ignore rank and regiment differences

--- src/soldiers/search/DefaultScoreFunction.java
+++ src/soldiers/search/DefaultScoreFunction.java
@@ -1,11 +1,13 @@
 package soldiers.search;
 
 public class DefaultScoreFunction implements ScoreFunction {
+	
+	// ignore differences in rank or regiment
 
 	@Override
 	public int calculate(CandidateScore score) {
 
-		int total = score.getNumber() + score.getSurname() + score.getInitials() + score.getRegiment() + score.getRank();
+		int total = score.getNumber() + score.getSurname() + score.getInitials();
 		return total;
 	}
 
