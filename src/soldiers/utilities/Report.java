8f861c5d5d928c112084a9a3e489d0ae0936cad5
tigersmuseum <85739505+tigersmuseum@users.noreply.github.com>
2023-03-31 11:56:33 +0100

    parameterized

--- src/soldiers/utilities/Report.java
+++ src/soldiers/utilities/Report.java
@@ -19,14 +19,22 @@ public class Report {
 	
 	public static void main(String[] args) throws TransformerConfigurationException, SAXException, IllegalArgumentException, FileNotFoundException  {
 
-		long sid = 114429;
+    	if ( args.length < 2 ) {
+    		
+    		System.err.println("Usage: Report <soldier-id> <output-filename>");
+    		System.exit(1);
+    	}
+
+		long sid = Long.valueOf(args[0]);
+		String outputfile = args[1];
+		
 		Person p = SoldiersModel.getPerson(ConnectionManager.getConnection(), sid);
 		
         SAXTransformerFactory tf = (SAXTransformerFactory) TransformerFactory.newInstance();
         TransformerHandler serializer;
         serializer = tf.newTransformerHandler();
         serializer.getTransformer().setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-        serializer.setResult(new StreamResult(new FileOutputStream("output/report.xml")));
+        serializer.setResult(new StreamResult(new FileOutputStream(outputfile)));
 
 		serializer.startDocument();
 		p.serializePerson(serializer);
