78938b4f1c6ef8486dc3371066622b42c545bc01
knoxa <swaffhamtown@outlook.com>
2024-04-02 15:53:24 +0100

    Exact name and number returns nothing if no number

--- src/soldiers/database/SoldiersModel.java
+++ src/soldiers/database/SoldiersModel.java
@@ -111,41 +111,44 @@ public class SoldiersModel {
 		
 		String sql = "select P.SID, P.SURNAME, P.INITIALS, P.FORENAMES, S.NUM, S.RANK_ABBREV, S.REGIMENT from PERSON P, SERVICE S where P.SURNAME = ? and P.SID = S.SID and S.NUM != '' and S.NUM like ?";
 
-		try {
+		Set<Service> service = person.getService();
+		Service svc = service.iterator().next();
+		
+		if (  svc.getNumber().length() > 0 ) { // there must be a number ...
+			
+			try {
 				
-			Set<Service> service = person.getService();
-			Service svc = service.iterator().next();
-			
-			PreparedStatement stmt = connection.prepareStatement(sql);
-			stmt.setString(1, person.getSurname());
-			stmt.setString(2, "%" + svc.getNumber());
-			
-			ResultSet results = stmt.executeQuery();
-			
-			while ( results.next() ) {
+				PreparedStatement stmt = connection.prepareStatement(sql);
+				stmt.setString(1, person.getSurname());
+				stmt.setString(2, "%" + svc.getNumber());
 				
-				Person candidate = new Person();
-				Service ss = new Service();
+				ResultSet results = stmt.executeQuery();
+				
+				while ( results.next() ) {
+					
+					Person candidate = new Person();
+					Service ss = new Service();
 
-				candidate.setSoldierId(results.getLong("SID"));
-				ss.setNumber(results.getString("NUM"));
-				ss.setRank(results.getString("RANK_ABBREV"));
-				ss.setRegiment(results.getString("REGIMENT"));
-				candidate.setSurname(results.getString("SURNAME"));
-				candidate.setInitials(results.getString("INITIALS"));
-				candidate.setForenames(results.getString("FORENAMES"));
-				
-				candidate.addService(ss);
+					candidate.setSoldierId(results.getLong("SID"));
+					ss.setNumber(results.getString("NUM"));
+					ss.setRank(results.getString("RANK_ABBREV"));
+					ss.setRegiment(results.getString("REGIMENT"));
+					candidate.setSurname(results.getString("SURNAME"));
+					candidate.setInitials(results.getString("INITIALS"));
+					candidate.setForenames(results.getString("FORENAMES"));
+					
+					candidate.addService(ss);
 
-				candidates.add(candidate);
+					candidates.add(candidate);
+				}
+				
+				stmt.close();
 			}
-			
-			stmt.close();
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-				
+						
 		return candidates;
 	}
 
