dc228e4c7fe8db1239a1e9b3a6af73becdbb258b
knoxa <swaffhamtown@outlook.com>
2025-12-04 15:26:31 +0000

    not comparable of either date is null

--- src/soldiers/utilities/ServiceComparator.java
+++ src/soldiers/utilities/ServiceComparator.java
@@ -1,5 +1,6 @@
 package soldiers.utilities;
 
+import java.sql.Date;
 import java.util.Comparator;
 
 import soldiers.database.Service;
@@ -7,7 +8,14 @@ import soldiers.database.Service;
 public class ServiceComparator implements Comparator<Service> {
 
 	public int compare(Service s1, Service s2) {
-		return s1.getBefore().compareTo(s2.getBefore());
+		Date s1date = s1.getBefore();
+		Date s2date = s2.getBefore();
+		
+		if (s1date != null && s2date != null ) {
+			
+			return s1.getBefore().compareTo(s2.getBefore());
+		}
+		else return 0;
 	}
 
 }
